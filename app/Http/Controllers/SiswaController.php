b85b7f45658d0ee6dbd95e8c4c35148237add041
usii90 <lusi_032@smkassalaambandung.sch.id>
2022-07-20 17:19:00 +0700

    first commit

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Siswa;
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //menampilkan semua data dari model Siswa
+        $siswa = Siswa::all();
+        return view('siswa.index', compact('siswa'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('siswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //validasi
+        $validated = $request->validate([
+            'nis' => 'required|unique:siswas|max:255',
+            'nama' => 'required',
+            'alamat' => 'required',
+            'tgl_lahir' => 'required',
+            
+        ]);
+
+        $siswa = new Siswa();
+        $siswa->nis = $request->nis;
+        $siswa->nama = $request->nama;
+        $siswa->alamat = $request->alamat;
+        $siswa->tgl_lahir = $request->tgl_lahir;
+        
+        $siswa->save();
+        return redirect()->route('siswa.index')
+            ->with('success', 'Data berhasil dibuat!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Siswa  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Siswa $siswa)
+    {
+        $siswa = Siswa::findOrFail($id);
+        return view('siswa.show', compact('siswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Siswa  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Siswa $siswa)
+    {
+        $siswa = Siswa::findOrFail($id);
+        return view('siswa.edit', compact('siswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Siswa  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Siswa $siswa)
+    {
+        // Validasi
+        $validated = $request->validate([
+            'nis' => 'required|max:255',
+            'nama' => 'required',
+            'alamat' => 'required',
+            'tgl_lahir' => 'required',
+            
+        ]);
+
+        $siswa = Siswa::findOrFail($id);
+        $siswa->nis = $request->nis;
+        $siswa->nama = $request->nama;
+        $siswa->alamat = $request->alamat;
+        $siswa->tgl_lahir = $request->tgl_lahir;
+        $siswa->save();
+        return redirect()->route('siswa.index')
+            ->with('success', 'Data berhasil diedit!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Siswa  $slot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Siswa $siswa)
+    {
+        $siswa = Siswa::findOrFail($id);
+        $siswa->delete();
+        return redirect()->route('siswa.index')
+            ->with('success', 'Data berhasil dihapus!');
+    }
+}
